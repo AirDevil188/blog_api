8071b810c418cad10265f9e7bcd2e5698a5be48d
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-11-01 09:32:09 +0100

    fix: turned on prettier

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,15 @@ dotenv.config();
 
 const app = express();
 
-app.use((err, req, res, next) => {
-    console.log(err);
-    return res.status(500).send(err)
-})
+const postRouter = require("./routes/postRouter");
 
-app.listen(process.env.PORT, () => console.log(`App is listening on the ${process.env.PORT}`))
+app.use("/posts", postRouter);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  return res.status(500).send(err);
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`App is listening on the ${process.env.PORT}`)
+);
