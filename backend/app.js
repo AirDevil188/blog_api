5b2c5426afca24da7d2cf6dcee21cc817919347a
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-11-04 22:53:26 +0100

    add: userRouter

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,15 @@ dotenv.config();
 const app = express();
 
 const postRouter = require("./routes/postRouter");
+const userRouter = require("./routes/userRouter");
+
+require("./config/passport");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/posts", postRouter);
+app.use("/", userRouter);
 
 app.use((err, req, res, next) => {
   console.log(err);
