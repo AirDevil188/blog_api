5518cf9aace8a9e377b8bf726a4b2b27b9c42fbc
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-11-07 23:25:31 +0100

    add: getPostDetails, createPost, getAllPosts middlewares

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,14 +1,30 @@
 const asyncHandler = require("express-async-handler");
+const db = require("../db/queries");
 
-const getAllPosts = asyncHandler((req, res, next) => {
-  res.send("GET Posts Route");
+const getAllPosts = asyncHandler(async (req, res, next) => {
+  const posts = await db.getAllPosts();
+  if (posts.length === 0) {
+    return res.json({ message: "There are no posts!" });
+  }
+  return res.json(posts);
 });
 
-const createPost = asyncHandler((req, res, next) => {
-  res.send("POST Posts Route");
+const getPostDetails = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const post = await db.getPost(id);
+  if (!post) {
+    return res.status(404).json({ message: "Post Not Found" });
+  }
+  return res.json(post);
+});
+
+const createPost = asyncHandler(async (req, res, next) => {
+  const { title, text } = req.body;
+  await db.createPost(title, text, req.user);
 });
 
 module.exports = {
-  getPosts,
-  postPosts,
+  getAllPosts,
+  createPost,
+  getPostDetails,
 };
