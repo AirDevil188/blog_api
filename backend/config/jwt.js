6c2e82cd3d07c36a50b6988688a5b1c63e28113b
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-11-08 17:12:27 +0100

    fix: change generateToken syntax

--- backend/config/jwt.js
+++ backend/config/jwt.js
@@ -4,16 +4,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const generateToken = async (user, res) => {
-  jwt.sign(
-    { user: user.id },
-    process.env.SECRET,
-    { expiresIn: "2min" },
-    (err, token) => {
-      return res.json({
-        token,
-      });
-    }
-  );
+  return jwt.sign({ user: user.id }, process.env.SECRET, { expiresIn: "2min" });
 };
 
 module.exports = { generateToken };
