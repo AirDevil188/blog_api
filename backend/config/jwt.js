dbd0199f511c20ace875b75e3d28033144c1d53a
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-11-11 00:07:30 +0100

    add jwt experation to 10min for testing

--- backend/config/jwt.js
+++ backend/config/jwt.js
@@ -4,7 +4,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const generateToken = async (user, res) => {
-  return jwt.sign({ user: user.id }, process.env.SECRET, { expiresIn: "2min" });
+  return jwt.sign({ user: user.id }, process.env.SECRET, {
+    expiresIn: "10min",
+  });
 };
 
 module.exports = { generateToken };
