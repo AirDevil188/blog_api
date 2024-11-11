2a1841518428eda7681b50bd761f5dca47eeaf5a
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-11-12 00:23:39 +0100

    add: role.check to postRouter

--- backend/routes/postRouter.js
+++ backend/routes/postRouter.js
@@ -4,6 +4,8 @@ const postRouter = Router({ mergeParams: true });
 const postController = require("../controllers/postController");
 const passport = require("passport");
 
+const roleCheck = require("../middlewares/roleCheck");
+
 postRouter.get("/", postController.getAllPosts);
 
 postRouter.get("/post/:id", postController.getPostDetails);
@@ -11,18 +13,21 @@ postRouter.get("/post/:id", postController.getPostDetails);
 postRouter.post(
   "/create-post",
   passport.authenticate("jwt", { session: false }),
+  roleCheck.roleCheck,
   postController.createPost
 );
 
 postRouter.delete(
   "/post/delete/:id",
   passport.authenticate("jwt", { session: false }),
+  roleCheck.roleCheck,
   postController.deletePost
 );
 
 postRouter.put(
   "/post/update/:id",
   passport.authenticate("jwt", { session: false }),
+  roleCheck.roleCheck,
   postController.updatePost
 );
 
