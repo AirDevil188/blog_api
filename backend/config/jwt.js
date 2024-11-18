7aa567ba0a4311405a2ba7612383f20b28b6219f
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-11-18 15:26:05 +0100

    add: username property to jwt object

--- backend/config/jwt.js
+++ backend/config/jwt.js
@@ -4,9 +4,13 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const generateToken = async (user, res) => {
-  return jwt.sign({ user: user.id, role: user.role }, process.env.SECRET, {
-    expiresIn: "10min",
-  });
+  return jwt.sign(
+    { user: user.id, role: user.role, username: user.username },
+    process.env.SECRET,
+    {
+      expiresIn: "10min",
+    }
+  );
 };
 
 module.exports = { generateToken };
