24d9bff35a1bd65412005eca1b1e845e2b614703
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-11-18 15:36:28 +0100

    add: contextHook for userObj and useEffect hook for validateJWT token

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,29 @@
-import { useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { validateJWT } from "./helper/validateJWT";
+
+export const UserContext = createContext(null);
+
+const App = () => {
+  const [userObject, setUserObject] = useState({ username: "", token: "" });
+
+  useEffect(() => {
+    const token = validateJWT();
+    if (token) {
+      setUserObject({
+        ...userObject,
+        username: token.username,
+        token: localStorage.getItem("token"),
+      });
+    }
+  }, []);
+  return (
+    <>
+      <UserContext.Provider value={{ userObject, setUserObject }}>
+        <Outlet />
+      </UserContext.Provider>
+    </>
+  );
+};
 
 export default App;
