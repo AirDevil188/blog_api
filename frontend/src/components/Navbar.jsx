24b7fe08e84c7960bee5e4d2cb7c8f0f7b7a0dd1
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-11-24 01:00:33 +0100

    fix: delete userObject state changed to props, added conditional rendering of li elements with logout

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,31 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useOutletContext } from "react-router-dom";
 
-const NavBar = () => {
+const NavBar = ({ user }) => {
+  const navigate = useNavigate();
+
+  const handleLogOut = () => {
+    localStorage.clear();
+    setUserObject(null);
+    navigate("/log-in");
+  };
   return (
     <>
       <header>
         <nav>
           <menu>
             <li>Home</li>
-            <li>Sign Up</li>
-            <li>Log In</li>
-            <li>Log Out</li>
+            <li>Posts</li>
+            {!user ? (
+              <>
+                <li>Log In</li>
+                <li>Sign Up</li>
+              </>
+            ) : (
+              <>
+                <li onClick={handleLogOut}>Log Out</li>
+              </>
+            )}
           </menu>
         </nav>
       </header>
