5b7f2508289c7e34db6298ef4d3697d17c24c65d
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-11-25 23:22:18 +0100

    fix: json return for all middlewares in the commentController

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -4,19 +4,19 @@ const db = require("../db/queries");
 const createComment = asyncHandler(async (req, res, next) => {
   console.log(req.user);
   const { text } = req.body;
-  await db.createComment(text, req.params.postId, req.user.user);
-  return res.redirect(`/posts/post/${req.params.postId}`);
+  const data = await db.createComment(text, req.params.postId, req.user.user);
+  return res.json(data);
 });
 
 const deleteComment = asyncHandler(async (req, res, next) => {
-  await db.deleteComment(req.params.id);
-  return res.redirect(`/posts/post/${req.params.id}`);
+  const data = await db.deleteComment(req.params.id);
+  return res.json(data);
 });
 
 const updateComment = asyncHandler(async (req, res, next) => {
   const { text } = req.body;
-  await db.editComment(req.params.id, text, req.params.postId);
-  res.redirect(`/posts/post/${req.params.postId}`);
+  const data = await db.editComment(req.params.id, text, req.params.postId);
+  return res.json(data);
 });
 
 module.exports = {
