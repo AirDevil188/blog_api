f11655176f3681125bbefc73c0f9da212627106e
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-11-28 01:30:59 +0100

    fix: getAllPosts middleware so that it returns error

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -4,7 +4,7 @@ const db = require("../db/queries");
 const getAllPosts = asyncHandler(async (req, res, next) => {
   const posts = await db.getAllPosts();
   if (posts.length === 0) {
-    return res.json({ message: "There are no posts!" });
+    return res.status(404).json({ message: "There are no posts!" });
   }
   return res.json(posts);
 });
