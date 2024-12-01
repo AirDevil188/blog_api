fd47c0574da1eb7c7418fd675b0bc572a65e2e63
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-12-01 23:40:28 +0100

    fix: roleCheck middleware

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,10 +1,17 @@
 const asyncHandler = require("express-async-handler");
+const db = require("../db/queries");
 
 const roleCheck = asyncHandler(async (req, res, next) => {
+  const comment = await db.getComment(req.params.id);
+  if (comment) {
+    if (req.user.user === comment.userId) {
+      return next();
+    }
+  }
   if (req.user.role !== "admin") {
     return res.status(401).json({ msg: "User is not an admin" });
   }
-  next();
+  return next();
 });
 
 module.exports = { roleCheck };
