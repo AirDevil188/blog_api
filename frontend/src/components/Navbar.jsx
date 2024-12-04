1ddde725e155ca9f6a51c511e1fdc0d2897941e3
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-12-04 16:21:53 +0100

    add: hamburger functinality and css module for NavBar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,24 +1,46 @@
-import { useContext, useEffect, useState } from "react";
-import { useNavigate, useOutletContext } from "react-router-dom";
+import styles from "./Navbar.module.css";
+import { RxHamburgerMenu } from "react-icons/rx";
+import useClickOutside from "../hooks/useClickOutside";
+import { Link } from "react-router-dom";
 
-const NavBar = ({ user, setUserObject }) => {
-  const navigate = useNavigate();
+const NavBar = ({ user, setUserObject, hamburger, setHamburger }) => {
   const handleLogOut = () => {
     localStorage.clear();
     setUserObject("");
     navigate("/log-in");
   };
+
+  const handleOutsideClick = () => {
+    setHamburger(false);
+  };
+
+  const ref = useClickOutside(handleOutsideClick);
+
   return (
     <>
-      <header>
-        <nav>
-          <menu>
-            <li>Home</li>
-            <li>Posts</li>
+      <header ref={ref}>
+        <button
+          className={styles.hamburgerBtn}
+          onClick={() => setHamburger(!hamburger)}
+        >
+          <RxHamburgerMenu size={30} />
+        </button>
+        <section className={styles.logoSection}>
+          <h1>A Blog</h1>
+        </section>
+        <nav className={styles.nav}>
+          <menu className={hamburger ? styles.menuOpen : styles.menuClose}>
+            <Link to={"/"} onClick={() => setHamburger(false)}>
+              <li>Home</li>
+            </Link>
             {!user ? (
               <>
-                <li>Log In</li>
-                <li>Sign Up</li>
+                <Link to={"/log-in"} onClick={() => setHamburger(false)}>
+                  <li>Log In</li>
+                </Link>
+                <Link to={"/sign-up"} onClick={() => setHamburger(false)}>
+                  <li>Sign Up</li>
+                </Link>
               </>
             ) : (
               <>
