865cf08e042a9c14e33fca0701ed821b1b9f15d3
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-12-05 13:23:23 +0100

    add: main container element

--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -5,31 +5,33 @@ import { Link } from "react-router-dom";
 
 const Posts = ({ posts }) => {
   return (
-    <section className="posts-section">
-      {posts
-        ? posts.map((post) => {
-            return (
-              <article className="post" id={post.id} key={post.id}>
-                <section className="post-title-section">
-                  <h3>{post.title}</h3>
-                </section>
-                <section className="post-text-section">
-                  <p>{post.text}</p>
-                </section>
-                <section className="post-details-section">
-                  <FaUser />
-                  <small>User: {post.user.username}</small>
-                  <small>
-                    <FaCalendarAlt />
-                    Created At:
-                    {new DateTime(post.createdAt).toLocaleString()}
-                  </small>
-                </section>
-              </article>
-            );
-          })
-        : null}
-    </section>
+    <main>
+      <section className="posts-section">
+        {posts
+          ? posts.map((post) => {
+              return (
+                <article className="post" id={post.id} key={post.id}>
+                  <section className="post-title-section">
+                    <h3>{post.title}</h3>
+                  </section>
+                  <section className="post-text-section">
+                    <p>{post.text}</p>
+                  </section>
+                  <section className="post-details-section">
+                    <FaUser />
+                    <small>User: {post.user.username}</small>
+                    <small>
+                      <FaCalendarAlt />
+                      Created At:
+                      {new DateTime(post.createdAt).toLocaleString()}
+                    </small>
+                  </section>
+                </article>
+              );
+            })
+          : null}
+      </section>
+    </main>
   );
 };
 
