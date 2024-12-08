8d2953099071b0a4e39517224aa52cd978dbbe00
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-12-08 17:56:34 +0100

    add: to cors origin, link for cms-admin

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     methods: "GET,PUT,POST,DELETE",
     optionsSuccessStatus: 204,
   })
