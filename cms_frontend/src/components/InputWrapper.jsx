862fdba98780990fb1339d5b3d4e5e1685f6f36e
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-12-15 00:04:15 +0100

    fix: propType checking, id params

--- cms_frontend/src/components/InputWrapper.jsx
+++ cms_frontend/src/components/InputWrapper.jsx
@@ -1,22 +1,37 @@
 import PropTypes from "prop-types";
 import styles from "../components/Form.module.css";
 
-const InputWrapper = ({ className, label, type, name, isRequired, id }) => {
+const InputWrapper = ({
+  className,
+  label,
+  type,
+  name,
+  isRequired,
+  id,
+  value,
+}) => {
   return (
     <div className={styles[className]}>
       <label htmlFor={name}>{label}</label>
-      <input type={type} required={isRequired} id={id} name={name} />
+      <input
+        type={type}
+        required={isRequired}
+        id={id}
+        name={name}
+        defaultValue={value}
+      />
     </div>
   );
 };
 
 InputWrapper.propTypes = {
-  className: PropTypes.string.isRequired,
+  className: PropTypes.string,
   label: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   isRequired: PropTypes.bool.isRequired,
   id: PropTypes.string.isRequired,
+  value: PropTypes.string,
 };
 
 export default InputWrapper;
