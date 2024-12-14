91bfae70adbaacb389f402daf52c4f8ab5557e37
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-12-15 00:06:44 +0100

    add: new routes

--- cms_frontend/src/router/Router.jsx
+++ cms_frontend/src/router/Router.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
 import LogIn from "../components/Log-In";
+import NewPost from "../components/NewPost";
+import Posts from "../components/Posts";
 
 const Router = () => {
   const router = createBrowserRouter([
@@ -9,8 +11,21 @@ const Router = () => {
       path: "/",
       children: [
         {
+          path: "/",
+          element: <Posts />,
+        },
+        {
           path: "/log-in",
           element: <LogIn />,
+        },
+        {
+          path: "/new-post",
+          element: <NewPost />,
+        },
+
+        {
+          path: "/posts/post/update/:id",
+          element: <NewPost />,
         },
       ],
     },
