57f6bc82f21200a4b60179bed97d4cc1c232fa95
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-12-15 22:59:46 +0100

    add: useEffect hook that sets userObject

--- cms_frontend/src/App.jsx
+++ cms_frontend/src/App.jsx
@@ -1,9 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
+import { validateJWT } from "./helper/validateJWT";
 
 const App = () => {
   const [userObject, setUserObject] = useState("");
   const [errors, setErrors] = useState(null);
+
+  useEffect(() => {
+    const token = validateJWT();
+    if (token) {
+      setUserObject({
+        ...userObject,
+        username: token.username,
+        token: localStorage.getItem("token"),
+      });
+      console.log(userObject);
+    }
+  }, []);
 
   return (
     <>
