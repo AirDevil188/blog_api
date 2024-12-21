ce12685c2916dcd3be4b1d91295620b8591ed789
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-12-21 02:08:24 +0100

    add: loader to routes

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -1,37 +1,42 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
 import Homepage from "../components/Homepage";
 import LogIn from "../components/Log-In";
 import SignUp from "../components/Sign-up";
-import PostDetails from "../components/PostDetails";
+import PostDetails, { handleSubmit } from "../components/PostDetails";
+import { getPostAndComments, getPosts } from "../utils/loaders";
 
 const Router = () => {
-  const router = createBrowserRouter([
-    {
-      element: <App />,
-      path: "/",
-      children: [
-        {
-          path: "/log-in",
-          element: <LogIn />,
-        },
-        {
-          path: "/",
-          element: <Homepage />,
-        },
-        {
-          path: "/sign-up",
-          element: <SignUp />,
-        },
-        {
-          path: "/posts/post/:id",
-          element: <PostDetails />,
-        },
-      ],
-    },
-  ]);
+  const router = createBrowserRouter(
+    [
+      {
+        element: <App />,
+        path: "/",
+        children: [
+          {
+            path: "/log-in",
+            element: <LogIn />,
+          },
+          {
+            path: "/",
+            element: <Homepage />,
+            loader: getPosts,
+          },
+          {
+            path: "/sign-up",
+            element: <SignUp />,
+          },
+          {
+            path: "/posts/post/:id",
+            element: <PostDetails />,
+            loader: getPostAndComments,
+            action: handleSubmit,
+          },
+        ],
+      },
+    ],
+    {}
+  );
   return (
     <>
       <RouterProvider router={router} />
