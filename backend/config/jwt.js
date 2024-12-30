a5daf8e87888e0c6744ea0f3b4f078c4bda052b5
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-12-30 16:47:21 +0100

    fix: duration of jwt token and deleted redudant role property

--- backend/config/jwt.js
+++ backend/config/jwt.js
@@ -5,10 +5,10 @@ dotenv.config();
 
 const generateToken = async (user, res) => {
   return jwt.sign(
-    { user: user.id, role: user.role, username: user.username },
+    { user: user.id, username: user.username },
     process.env.SECRET,
     {
-      expiresIn: "10min",
+      expiresIn: "4h",
     }
   );
 };
