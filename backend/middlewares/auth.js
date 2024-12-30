6747282a5b31d9818e3912d6f2a8853d36c1e7a0
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-12-30 16:47:56 +0100

    fix: auth middleware

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,15 +2,18 @@ const asyncHandler = require("express-async-handler");
 const db = require("../db/queries");
 
 const roleCheck = asyncHandler(async (req, res, next) => {
-  const comment = await db.getComment(req.params.id);
-  if (comment) {
-    if (req.user.user === comment.userId) {
-      return next();
+  if (req.params.id) {
+    const comment = await db.getComment(req.params.id);
+    if (comment) {
+      if (req.user.user === comment.userId) {
+        return next();
+      }
+    }
+    if (req.user.role !== "admin") {
+      return res.status(401).json({ msg: "User is not an admin" });
     }
   }
-  if (req.user.role !== "admin") {
-    return res.status(401).json({ msg: "User is not an admin" });
-  }
+
   return next();
 });
 
