7f22b4b98d6eef42395b018310964bc9cf903851
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-01-01 00:27:01 +0100

    fix: refactored code added action function

--- cms_frontend/src/components/Log-In.jsx
+++ cms_frontend/src/components/Log-In.jsx
@@ -1,81 +1,53 @@
-import { useEffect } from "react";
-import { jwtDecode } from "jwt-decode";
-import { useNavigate, useOutletContext } from "react-router-dom";
+import { useFetcher, useNavigate, useOutletContext } from "react-router-dom";
 import styles from "../components/Log-In.module.css";
-import Button from "./Button";
-import Form from "./Form";
-import InputWrapper from "./InputWrapper";
+import { handleFetch } from "../utils/handleFetch";
+import { useEffect } from "react";
 const LogIn = () => {
   const navigate = useNavigate();
+  const fetcher = useFetcher();
   const {
     userObject: [userObject, setUserObject],
     errors: [errors, setErrors],
   } = useOutletContext();
+
   useEffect(() => {
-    if (userObject) {
+    if (userObject.token) {
       navigate("/");
     }
-  });
+  }, [userObject.token]);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    try {
-      const response = await fetch("http://localhost:3000/cms-log-in", {
-        mode: "cors",
-        method: "POST",
-        body: JSON.stringify({
-          username: formData.get("username"),
-          password: formData.get("password"),
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const data = await response.json();
-
-      if (response.ok) {
-        const user = {
-          user: {
-            token: data.token,
-            username: formData.get("username"),
-          },
-        };
-        localStorage.setItem("token", user.user.token);
-        jwtDecode(localStorage.getItem("token"));
-        setUserObject(user);
-        console.log(userObject);
-        navigate("/");
-      }
-      setErrors({ message: data.message });
-    } catch (err) {
-      console.log(err);
-      return err;
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      setErrors(fetcher.data);
+      return;
     }
-  };
+    if (fetcher.data) {
+      setUserObject({
+        ...userObject,
+        token: fetcher.data.token,
+      });
+      navigate("/");
+    }
+  }, [fetcher.data]);
+
   return (
     <>
       <main className={styles.mainContainer}>
         <section className={styles.logInSection}>
-          <Form onSubmit={handleSubmit} method={"post"}>
-            <InputWrapper
-              label={"Username: "}
-              id={"username"}
-              name={"username"}
-              type={"text"}
-              isRequired={true}
-              className={"form-group"}
-            ></InputWrapper>
-            <InputWrapper
-              label={"Password: "}
-              id={"password"}
-              name={"password"}
-              type={"password"}
-              isRequired={true}
-              className={"form-group"}
-            ></InputWrapper>
-            <Button text={"Submit"} type={"submit"} />
-          </Form>
+          <fetcher.Form method="POST">
+            <div className={styles.formGroup}>
+              <label htmlFor="username">Username: </label>
+              <input type="text" name="username" id="username" required />
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="password">Password: </label>
+              <input type="password" name="password" id="password" required />
+            </div>
+            <div className="button-container">
+              <button type="submit">Log In</button>
+            </div>
+          </fetcher.Form>
+
           {errors ? (
             <>
               <section className="error-section">
@@ -89,6 +61,28 @@ const LogIn = () => {
       </main>
     </>
   );
+};
+
+export const handleSubmitLogIn = async ({ request }) => {
+  const formData = await request.formData();
+  const submission = {
+    username: formData.get("username"),
+    password: formData.get("password"),
+  };
+  const res = await handleFetch(`/log-in`, submission, "post");
+  if (res.ok) {
+    const data = await res.json();
+    const user = {
+      user: {
+        token: data.token,
+        username: formData.get("username"),
+      },
+    };
+    localStorage.setItem("token", user.user.token);
+    return data;
+  } else {
+    return await res.json();
+  }
 };
 
 export default LogIn;
