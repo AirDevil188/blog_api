2d820e98a64091bff8215d04679eda0475ebba74
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-01-01 00:21:13 +0100

    add: propTypeChecking and added more props to component with css module

--- cms_frontend/src/components/Button.jsx
+++ cms_frontend/src/components/Button.jsx
@@ -1,9 +1,16 @@
 import styles from "../components/Button.module.css";
 import PropTypes from "prop-types";
 
-const Button = ({ text, className, type, id, onClick }) => {
+const Button = ({ text, className, type, id, onClick, name, value }) => {
   return (
-    <button className={styles[className]} type={type} onClick={onClick} id={id}>
+    <button
+      className={styles[className]}
+      type={type}
+      onClick={onClick}
+      id={id}
+      name={name}
+      value={value}
+    >
       {text}
     </button>
   );
@@ -15,6 +22,8 @@ Button.propTypes = {
   type: PropTypes.string.isRequired,
   onClick: PropTypes.func,
   id: PropTypes.string,
+  name: PropTypes.string,
+  value: PropTypes.string,
 };
 
 export default Button;
