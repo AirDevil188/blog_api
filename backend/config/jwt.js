1d185bded5dfec77caf530e63e6c6ca5c7235a0d
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-01-01 00:33:22 +0100

    added role to jwt.sign

--- backend/config/jwt.js
+++ backend/config/jwt.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const generateToken = async (user, res) => {
   return jwt.sign(
-    { user: user.id, username: user.username },
+    { user: user.id, username: user.username, role: user.role },
     process.env.SECRET,
     {
       expiresIn: "4h",
