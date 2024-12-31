7cfab7fab0bf076ce95cae2cabfcf20b14cfc1b2
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-01-01 00:28:49 +0100

    fix: refactored code, added actions

--- cms_frontend/src/components/NewPost.jsx
+++ cms_frontend/src/components/NewPost.jsx
@@ -1,111 +1,37 @@
-import { useEffect, useState } from "react";
 import styles from "../components/NewPost.module.css";
 import NewPostCard from "./NewPostForm";
-import { useNavigate, useOutletContext, useParams } from "react-router-dom";
+import { redirect, useLoaderData } from "react-router-dom";
+import { handleFetch } from "../utils/handleFetch";
 
 const NewPost = () => {
-  const params = useParams();
-  const {
-    userObject: [userObject],
-    errors: [errors, setErrors],
-  } = useOutletContext();
-  const navigate = useNavigate();
-  const [edit, setEdit] = useState(null);
-  const [isFetching, setIsFetching] = useState(false);
-
-  useEffect(() => {
-    setIsFetching(true);
-
-    const fetchPost = async () => {
-      setIsFetching(true);
-      if (isFetching) {
-        if (Object.hasOwn(params, "id")) {
-          const response = await fetch(
-            `http://localhost:3000/posts/post/update/${params.id}`,
-            {
-              mode: "cors",
-              method: "GET",
-              headers: {
-                "Content-Type": "application/json",
-                Authorization: "Bearer " + userObject.token,
-              },
-            }
-          );
-          const data = await response.json();
-          if (response.ok) {
-            setEdit(data);
-            return;
-          }
-          setErrors(data);
-        }
-      } else return;
-    };
-    fetchPost();
-  }, [isFetching]);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const text = formData.get("text");
-    const response = await fetch(`http://localhost:3000/posts/create-post`, {
-      mode: "cors",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + userObject.token,
-      },
-      body: JSON.stringify({
-        title: formData.get("title"),
-        text: text,
-      }),
-    });
-    const data = await response.json();
-    if (response.ok) {
-      navigate("/");
-      return;
-    }
-    setErrors({ message: data.message });
-  };
-
-  const handleEdit = async (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const response = await fetch(
-      `http://localhost:3000/posts/post/update/${params.id}`,
-      {
-        mode: "cors",
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + userObject.token,
-        },
-        body: JSON.stringify({
-          title: formData.get("title"),
-          text: formData.get("text"),
-        }),
-      }
-    );
-    const data = await response.json();
-    if (response.ok) {
-      navigate("/");
-      return;
-    }
-    setErrors({
-      message: data.message,
-    });
-  };
+  const edit = useLoaderData();
 
   return (
     <>
       <section className={styles.newPostSection}>
-        <NewPostCard
-          errors={errors}
-          handleSubmit={edit ? handleEdit : handleSubmit}
-          edit={edit}
-        />
+        <NewPostCard edit={edit} />
       </section>
     </>
   );
+};
+
+export const handleAction = async ({ request, params }) => {
+  const data = await request.formData();
+  const submission = {
+    title: data.get("title"),
+    text: data.get("text"),
+  };
+  const { id } = params;
+
+  switch (id) {
+    case undefined:
+      await handleFetch(`/posts/create-post`, submission, "POST");
+      return redirect(`/`);
+
+    default:
+      await handleFetch(`/posts/post/update/${id}`, submission, "PUT");
+      return redirect(`/posts/post/${id}`);
+  }
 };
 
 export default NewPost;
--- cms_frontend/src/components/NewPostForm.jsx
+++ cms_frontend/src/components/NewPostForm.jsx
@@ -1,81 +1,69 @@
 import PropTypes from "prop-types";
 import Button from "./Button";
-import Form from "./Form";
 import InputWrapper from "./InputWrapper";
 import { Editor } from "@tinymce/tinymce-react";
 import styles from "../components/Button.module.css";
+import { useFetcher } from "react-router-dom";
 
-const NewPostCard = ({ errors, handleSubmit, edit }) => {
+const NewPostCard = ({ edit }) => {
+  const fetcher = useFetcher();
   return (
     <>
-      {errors ? (
-        <>
-          <>
-            <section className="error-section">
-              <div className="error-container">
-                <p>{errors.message}</p>
-              </div>
-            </section>
-          </>
-        </>
-      ) : (
-        <>
-          <h3>{edit ? "Edit Post" : "Create Post"}</h3>
-          <Form onSubmit={handleSubmit} method={"POST"}>
-            <InputWrapper
-              label={"Title: "}
-              type={"text"}
-              id={"title"}
-              name={"title"}
-              isRequired={true}
-              value={edit ? edit.title : ""}
-            />
-            <Editor
-              textareaName="text"
-              apiKey="ubhgfcbqf9gpyz0tb59e3o6q8ohvmex2q53zyoi14bbsmbpm"
-              init={{
-                plugins: [
-                  // Core editing features
-                  "anchor",
-                  "autolink",
-                  "charmap",
-                  "codesample",
-                  "emoticons",
-                  "image",
-                  "link",
-                  "lists",
-                  "media",
-                  "searchreplace",
-                  "table",
-                  "visualblocks",
-                  "wordcount",
-                ],
-                toolbar:
-                  "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
-                tinycomments_mode: "embedded",
-                tinycomments_author: "Author name",
-                mergetags_list: [
-                  { value: "First.Name", title: "First Name" },
-                  { value: "Email", title: "Email" },
-                ],
-              }}
-              initialValue={edit ? edit.text : "Write post..."}
-            />
+      <>
+        <h3>{edit ? "Edit Post" : "Create Post"}</h3>
+        <fetcher.Form method={"POST"}>
+          <InputWrapper
+            label={"Title: "}
+            type={"text"}
+            id={"title"}
+            name={"title"}
+            isRequired={true}
+            value={edit ? edit.title : ""}
+          />
+          <Editor
+            textareaName="text"
+            apiKey="ubhgfcbqf9gpyz0tb59e3o6q8ohvmex2q53zyoi14bbsmbpm"
+            init={{
+              plugins: [
+                // Core editing features
+                "anchor",
+                "autolink",
+                "charmap",
+                "codesample",
+                "emoticons",
+                "image",
+                "link",
+                "lists",
+                "media",
+                "searchreplace",
+                "table",
+                "visualblocks",
+                "wordcount",
+              ],
+              toolbar:
+                "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
+              tinycomments_mode: "embedded",
+              tinycomments_author: "Author name",
+              mergetags_list: [
+                { value: "First.Name", title: "First Name" },
+                { value: "Email", title: "Email" },
+              ],
+            }}
+            initialValue={edit ? edit.text : "Write post..."}
+          />
 
-            <Button
-              type={"submit"}
-              text={"Submit"}
-              className={styles.btnSubmit}
-            ></Button>
-          </Form>
-        </>
-      )}
+          <Button
+            type={"submit"}
+            text={"Submit"}
+            className={styles.btnSubmit}
+          ></Button>
+        </fetcher.Form>
+      </>
     </>
   );
 };
 
 NewPostCard.propTypes = {
-  handleSubmit: PropTypes.func.isRequired,
   errors: PropTypes.object,
   edit: PropTypes.object,
 };
