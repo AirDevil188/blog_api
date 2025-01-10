b1a2186f2ba8f7be7830d12e260a35b872dc2a20
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-01-10 17:37:12 +0100

    add: posts draft section

--- cms_frontend/src/components/Posts.jsx
+++ cms_frontend/src/components/Posts.jsx
@@ -1,66 +1,98 @@
-import { redirect, useLoaderData, useFetcher } from "react-router-dom";
+import {
+  redirect,
+  useLoaderData,
+  useFetcher,
+  useOutletContext,
+  useNavigate,
+} from "react-router-dom";
 import styles from "../components/Posts.module.css";
 import { DateTime } from "luxon";
 import Button from "./Button";
 import { handleFetch } from "../utils/handleFetch";
+import { RiDraftLine } from "react-icons/ri";
+import { useEffect } from "react";
 
 const Posts = () => {
   const posts = useLoaderData();
   const fetcher = useFetcher();
+  const {
+    userObject: [userObject],
+  } = useOutletContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!userObject.token) {
+      navigate("/log-in");
+    }
+  });
 
   return (
     <>
       <main className={styles.mainContainer}>
-        <section className={styles.titleSection}>
-          <h3>Posts: </h3>
-        </section>
         <section className={styles.postsSection}>
-          {posts
-            ? posts.map((post) => {
-                return (
-                  <a
-                    className={styles.post}
-                    id={post.id}
-                    key={post.id}
-                    href={`posts/post/${post.id}`}
-                  >
-                    <section className="post-title-section">
-                      <h3>{post.title}</h3>
-                    </section>
+          <section className={styles.postsHeader}>
+            <span>Title</span>
+            <span>Author</span>
+            <span>Categories</span>
+            <span>Buttons</span>
+            <span></span>
+          </section>
+          <section className={styles.posts}>
+            {posts
+              ? posts.map((post) => {
+                  return (
+                    <a
+                      className={styles.post}
+                      id={post.id}
+                      key={post.id}
+                      href={`posts/post/${post.id}`}
+                    >
+                      <section className="post-title-section">
+                        <h3>{post.title}</h3>
+                        {!post.publish ? (
+                          <>
+                            <div className="post-draft">
+                              <small>Draft</small>
+                              <RiDraftLine />
+                            </div>
+                          </>
+                        ) : null}
+                      </section>
 
-                    <section className="post-details-section">
-                      <div>
-                        <small>User: {post.user.username} </small>
-                      </div>
-                      <small>
-                        Created At:
-                        {new DateTime(post.createdAt).toLocaleString()}
-                      </small>
-                    </section>
-                    <section className="post-buttons-section">
-                      <fetcher.Form method="POST">
-                        <input type="hidden" name="id" value={post.id} />
+                      <section className="post-details-section">
+                        <div>
+                          <small>User: {post.user.username} </small>
+                        </div>
+                        <small>
+                          Created At:
+                          {new DateTime(post.createdAt).toLocaleString()}
+                        </small>
+                      </section>
+                      <section className={styles.postButtonsSection}>
+                        <fetcher.Form method="POST">
+                          <input type="hidden" name="id" value={post.id} />
 
-                        <Button
-                          text={"EDIT"}
-                          type={"submit"}
-                          value={"edit"}
-                          name={"intent"}
-                          id={post.id}
-                        ></Button>
-                        <Button
-                          text={"DELETE"}
-                          type={"submit"}
-                          value={"delete"}
-                          name={"intent"}
-                          id={post.id}
-                        ></Button>
-                      </fetcher.Form>
-                    </section>
-                  </a>
-                );
-              })
-            : null}
+                          <Button
+                            text={"EDIT"}
+                            type={"submit"}
+                            value={"edit"}
+                            name={"intent"}
+                            id={post.id}
+                          ></Button>
+                          <Button
+                            text={"DELETE"}
+                            type={"submit"}
+                            value={"delete"}
+                            name={"intent"}
+                            id={post.id}
+                          ></Button>
+                        </fetcher.Form>
+                      </section>
+                    </a>
+                  );
+                })
+              : null}
+          </section>
         </section>
       </main>
     </>
@@ -72,7 +104,6 @@ export const handleSubmit = async ({ request }) => {
   const formData = Object.fromEntries(data);
 
   const button = data.get("intent");
-  console.log(button);
 
   switch (button) {
     case "edit":
