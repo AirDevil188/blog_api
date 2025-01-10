82f66f2f436334e3359a5e28dce1d61742b98b58
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-01-10 15:15:13 +0100

    add: new routes, and changed actions

--- cms_frontend/src/router/Router.jsx
+++ cms_frontend/src/router/Router.jsx
@@ -2,10 +2,12 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
 import LogIn, { handleSubmitLogIn } from "../components/Log-In";
 import NewPost, { handleAction } from "../components/NewPost";
+import NewCategory, { handleCategorySubmit } from "../components/NewCategory";
 import Posts, { handleSubmit } from "../components/Posts";
 import PostDetails, { handleDelete } from "../components/PostDetails";
-import { getPost, getPosts, updatePost } from "../utils/loaders";
+import { getCategories, getPost, getPosts, updatePost } from "../utils/loaders";
 import Error from "../components/ErrorElement";
+import UpdatePost, { handleUpdateAction } from "../components/UpdatePost";
 
 const Router = () => {
   const router = createBrowserRouter([
@@ -28,20 +30,32 @@ const Router = () => {
         {
           path: "/new-post",
           element: <NewPost />,
+          loader: getCategories,
           action: handleAction,
         },
 
         {
           path: "/posts/post/update/:id",
-          element: <NewPost />,
+          element: <UpdatePost />,
           loader: updatePost,
-          action: handleAction,
+          action: handleUpdateAction,
         },
         {
           path: "/posts/post/:id",
           element: <PostDetails />,
           loader: getPost,
           action: handleDelete,
+        },
+
+        {
+          path: "/new-category",
+          element: <NewCategory />,
+          action: handleCategorySubmit,
+        },
+
+        {
+          path: "/categories",
+          loader: getCategories,
         },
       ],
     },
