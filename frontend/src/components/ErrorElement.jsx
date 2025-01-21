bdb63f1d1acfff37b995d55fb68b200ecb4bc276
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-01-21 16:59:57 +0100

    fix: changed color of a href text

--- frontend/src/components/ErrorElement.jsx
+++ frontend/src/components/ErrorElement.jsx
@@ -1,13 +1,23 @@
 import { useRouteError } from "react-router-dom";
+import styles from "./ErrorElement.module.css";
 
 const Error = () => {
   const error = useRouteError();
 
   return (
     <>
-      <main>
-        <section className="error-section">
-          <p>{error.message}</p>
+      <main className={styles.mainContainer}>
+        <section className={styles.errorSection}>
+          {error.message === "Posts Not Found" ? (
+            <>
+              <span>{error.message}</span>
+              <div className={styles.errorLink}>
+                <a href="/new-post">New Post</a>
+              </div>
+            </>
+          ) : (
+            <span>{error.message}</span>
+          )}
         </section>
       </main>
     </>
