0e03445d52a941db5f1ea667591aa519610b2134
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-01-27 13:56:39 +0100

    fix: handleFetch production

--- cms_frontend/src/utils/handleFetch.jsx
+++ cms_frontend/src/utils/handleFetch.jsx
@@ -1,6 +1,8 @@
 const host =
   import.meta.env.MODE === "development" ? "http://localhost:3000" : "http";
 
+console.log(host);
+
 export const handleFetch = async (endPoint, input = undefined, method) => {
   const token = localStorage.getItem("token");
   const options = {
--- frontend/src/utils/handleFetch.jsx
+++ frontend/src/utils/handleFetch.jsx
@@ -1,4 +1,7 @@
-const host = import.meta.env.MODE === "" ? "http://localhost:3000" : "http";
+const host =
+  import.meta.env.MODE === "development" ? "http://localhost:3000" : "http";
+
+console.log(host);
 
 export const handleFetch = async (endPoint, input = undefined, method) => {
   const token = localStorage.getItem("token");
