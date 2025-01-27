2e4b65a37a00f8317795e71aa2fa89045883668e
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-01-27 14:21:30 +0100

    fix: host variable

--- frontend/src/utils/handleFetch.jsx
+++ frontend/src/utils/handleFetch.jsx
@@ -1,7 +1,7 @@
 const host =
-  import.meta.env.MODE === "development" ? "http://localhost:3000" : "https";
-
-console.log(host);
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000"
+    : window.location.host;
 
 export const handleFetch = async (endPoint, input = undefined, method) => {
   const token = localStorage.getItem("token");
