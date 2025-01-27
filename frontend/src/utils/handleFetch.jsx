be39a6a37a96685857df570f1730cf55afd5e05d
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-01-27 14:07:00 +0100

    fix: host variable in handleFetch utils. removed redudant console.logs

--- frontend/src/utils/handleFetch.jsx
+++ frontend/src/utils/handleFetch.jsx
@@ -1,5 +1,5 @@
 const host =
-  import.meta.env.MODE === "development" ? "http://localhost:3000" : "http";
+  import.meta.env.MODE === "development" ? "http://localhost:3000" : "https";
 
 console.log(host);
 
